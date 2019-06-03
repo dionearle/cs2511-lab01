2c997ca4a627a600967441ad518bf442264ad3a7
Dion Earle <dion.earle@gmail.com>
2019-06-03 15:39:00 +1000

    Welcome to java comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,5 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Welcome to COMP2511!");    }
+        System.out.println("Hello, Welcome to Java!");    }
 }
