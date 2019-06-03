c8354667dee4c5af3bac47fbbadaa0805686892e
Dion Earle <dion.earle@gmail.com>
2019-06-03 16:01:17 +1000

    Removing duplicate HelloWorld.java file

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
-* A simple java program that prints a hello world message to the console
-*
-*/
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, Welcome to Java!");    }
-}
