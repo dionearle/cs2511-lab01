0849a918df6ed732eee03ba6dd5c7c697c8d3eb9
Dion Earle <dion.earle@gmail.com>
2019-06-05 12:40:26 +1000

    Completed average array of numbers and splitting up words exercises

--- src/example/Average.java
+++ src/example/Average.java
@@ -11,13 +11,28 @@ public class Average {
          * @param the array of integer numbers
          * @return the average of the numbers
          */
-        public float average(int[] nums) {
+        public float computeAverage(int[] nums) {
             float result = 0;
-            // Add your code
+            int sum = 0;
+            int i;
+            for(i = 0; i < nums.length; i++) {
+            	sum += nums[i];
+            }
+            result = (sum / nums.length);
             return result;
         }
 
         public static void main(String[] args) {
-            // Add your code
+
+        	int[] array;
+        	array = new int[3];
+        	
+        	array[0] = 1;
+        	array[1] = 2;
+        	array[2] = 3;
+        	Average average = new Average();
+			float answer = average.computeAverage(array);
+        	
+        	System.out.print(answer);
         }
 }
--- src/example/Splitter.java
+++ src/example/Splitter.java
@@ -1,9 +1,19 @@
 package example;
 
+import java.util.Scanner;
+
 public class Splitter {
 
     public static void main(String[] args){
         System.out.println("Enter a sentence specified by spaces only: ");
-        // Add your code
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(scanner.next());
+        while (scanner.hasNext()) {
+        	System.out.print('\n');
+        	System.out.print(scanner.next());
+        }
+        
+        scanner.close();
     }
 }
